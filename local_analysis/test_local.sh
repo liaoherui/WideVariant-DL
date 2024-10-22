e60108cfa545f286bbd74348ab73b5ff70801816
Ray <851836818@qq.com>
2024-10-22 10:39:43 -0400

    Add files via upload

---
+++ local_analysis/test_local.sh
@@ -0,0 +1,14 @@
+# Default test Cae datasets
+#python new_snv_script.py -i ../npz_of_Test_data/cae_pe/candidate_mutation_table/group_subj-5_candidate_mutation_table.npz -c ../npz_of_Test_data/cae_pe/candidate_mutation_table/group_subj-5_coverage_matrix_raw.npz -r reference_genomes/Cae_ref -o cae_pe_local
+
+# Kcp Science
+python new_snv_script.py -i ../../../Other_datasets/ScienceTM_KCp/npz_files_from_server/S20/group_Kcp_candidate_mutation_table.npz -c ../../../Other_datasets/ScienceTM_KCp/npz_files_from_server/S20/group_Kcp_coverage_matrix_raw.npz -r reference_genomes/Kcp_ref -o kcp_science_local_new
+
+# Cdi Plos
+#python new_snv_script.py -i ../../../Other_datasets/Plos-One-Cdi-2024/npz_from_servers/group_Plos-Cdi_candidate_mutation_table.npz -c ../../../Other_datasets/Plos-One-Cdi-2024/npz_from_servers/group_Plos-Cdi_coverage_matrix_raw.npz -r reference_genomes/Cdi_plos_ref -o cdi_plos_local_new
+
+# Sau eLife
+#python new_snv_script.py -i ../../../Other_datasets/eLife-Sau-2022/npz_files_from_server/S16/group_elife-Sau_candidate_mutation_table.npz -c ../../../Other_datasets/eLife-Sau-2022/npz_files_from_server/S16/group_elife-Sau_coverage_matrix_raw.npz -r reference_genomes/elife-Sau-ref -o sau_elife_local_new
+
+# Sau PNAS 
+#python new_snv_script.py -i  ../../../Other_datasets/PNAS-Sau-2014/npz_files_from_server/S121/group_pnas-Sau_candidate_mutation_table.npz -c ../../../Other_datasets/PNAS-Sau-2014/npz_files_from_server/S121/group_pnas-Sau_coverage_matrix_raw.npz -r reference_genomes/pnas_Sau-ref -o sau_pnas_local_new
